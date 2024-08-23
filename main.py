c549d3fad1a080bd4a0bc10c23b240a0e467e197
florejos001 <florejos001@warren.k12.in.us>
2024-08-23 10:03:44 -0400

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+miles = 5 #pretend this came from user input
+minutes = 45 #pretend this came from user input
+hours = minutes / 60
+
+mph = miles / hours
+
+print("How many miles did you run?", miles)
+print("For how long did you run?", minutes)
+print("Speed in mph", mph)
+
